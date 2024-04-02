3a711ab6441601808222a625dd6eafc6de817337
saydullayev-d <saydullayevdamir03@gmail.com>
2024-04-03 00:39:55 +0500

    first commit

---
+++ lib/core/resources/app_strings.dart
@@ -0,0 +1,37 @@
+class AppStrings {
+  static const String appTitle = 'Movies App';
+  static const String seeAll = 'посмотреть все';
+  static const String popularMovies = 'Популярные фильмы';
+  static const String topRatedMovies = 'Лучшие фильмы';
+  static const String story = 'История';
+  static const String videos = 'Видео';
+  static const String cast = 'Состав исполнителей';
+  static const String reviews = 'Отзывы';
+  static const String similar = 'Похожие';
+  static const String showLess = 'Показать меньше';
+  static const String showMore = 'Показать еще';
+  static const String movies = 'Фильмы';
+  static const String shows = 'Сериалы';
+  static const String search = 'Поиск';
+  static const String watchlist = 'Сохранённые';
+  static const String popularShows = 'Популярные передачи';
+  static const String topRatedShows = 'Самые рейтинговые сериалы';
+  static const String lastEpisodeOnAir = 'Последняя серия в эфире';
+  static const String seasons = 'Сезоны';
+  static const String season = 'Сезон';
+  static const String episodes = 'Эпизоды';
+  static const String episode = 'Эпизод';
+  static const String airDate = 'Дата эфира:';
+  static const String lastEpisode = 'Последний эпизод';
+  static const String searchText =
+      'Набрав в строке поиска, Movia ищет фильмы и сериалы, а затем показывает вам лучшие результаты.';
+  static const String searchHint = 'Поиск фильмов, сериалов...';
+  static const String watchlistIsEmpty = 'Сохранённые пусты';
+  static const String watchlistText =
+      'После добавления фильмов и сериалов в список просмотра они появятся здесь.';
+  static const String oops = 'Упс';
+  static const String tryAgainLater = 'Пожалуйста, повторите попытку позже';
+  static const String errorMessage = 'Что-то пошло не так';
+  static const String tryAgain = 'попробуйте ещё раз';
+  static const String noResults = 'Нет результатов';
+}
